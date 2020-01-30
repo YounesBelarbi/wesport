6e0e271958f0f8aedaf84da51160f69b1f021d01
younes <ybelarbi@outlook.com>
2020-01-30 10:19:25 +0100

    form layout

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,8 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class UserType extends AbstractType
 {
@@ -13,13 +17,39 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('password')
             ->add('username')
             ->add('age')
             ->add('lastName')
             ->add('firstName')
             ->add('city')
             ->add('phoneNumber')
+            ->add('oldPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'label' => 'Mot de passe actuelle ',
+                'required' => false
+         
+               
+            ])
+            ->add('newPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mots de passe doit contenir au minimum {{ limit }} caractères',
+                        'max' => 4096,
+                    ])
+                ],
+                'first_options'  => ['label' => 'Nouveau mot de passe'],
+                'second_options' => ['label' => 'Confirmer le nouveau mot de passe'],
+                'invalid_message' => 'Les mots de passe doivent être identiques',
+            ])
+            ->add('send', SubmitType::class, [
+                'label' => 'modifier'
+            ])
            
         ;
     }
