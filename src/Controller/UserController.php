d098ab3a479f19565a2f6f7afafae78d0d578029
younes <ybelarbi@outlook.com>
2020-01-30 10:59:25 +0100

    password modification process

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -32,6 +32,35 @@ class UserController extends AbstractController
 
         $form = $this->createForm(UserType::class, $user);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            //password processing
+            $oldPassword = $form->get('oldPassword')->getData();
+            $newPassword = $form->get('newPassword')->getData();
+            
+            
+            if (!empty(trim($newPassword)) && !empty($oldPassword)) {
+
+                $checkOldPassword = $passwordEncoder->isPasswordValid($user, $oldPassword);
+                
+                if($checkOldPassword) {   
+
+                        $user->setPassword(
+                            $passwordEncoder->encodePassword(
+                                $user,
+                                $form->get('newPassword')->getData()
+                            ));
+                    }         
+            }
+
+
+            $em->flush();
+            $this->addFlash('success', 'Votre profile à été mit à jour');
+        }
+            
+
         return $this->render('profileUser/update_profile.html.twig', [
             'form' => $form->createView(),
         ]);
