2b45c98ae5e5e10a99e977272b8e3e92c225d594
younes <ybelarbi@outlook.com>
2020-02-04 20:17:14 +0100

    try/catch to persist favoriteSport

--- src/Controller/UserFavoriteSportController.php
+++ src/Controller/UserFavoriteSportController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\FavoriteSport;
 use App\Form\FavoriteSportType;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,8 +12,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserFavoriteSportController extends AbstractController
 {
+
     /**
-     * @Route("/user/favorite/sport", name="user_favorite_sport")
+     * @Route("/user/addfavorite/sport", name="user_favorite_sport")
      */
     public function index(Request $request, EntityManagerInterface $em)
     {
@@ -25,17 +27,31 @@ class UserFavoriteSportController extends AbstractController
         $favoriteSportForm->handleRequest($request);
        
         if ($favoriteSportForm->isSubmitted() && $favoriteSportForm->isValid()) {
-            // dd($favoriteSportForm->get('level')->getData());
-            $favoriteSport = new FavoriteSport;
             
-            $favoriteSport->setLevel($favoriteSportForm->get('level')->getData());
-           
-            $favoriteSport->setUser($user);
-            $favoriteSport->setSport($favoriteSportForm->get('sport')->getData());
+            $favoriteSport = new FavoriteSport;
+            $level = $favoriteSportForm->get('level')->getData();
+            $sport = $favoriteSportForm->get('sport')->getData();
 
-            $em->persist($favoriteSport);
-            $em->flush();
-            $this->addFlash('success', 'Le sport est rajouté dans vos favoris');
+            $favoriteSport->setLevel($level);
+            $favoriteSport->setUser($user);
+            $favoriteSport->setSport($sport);
+
+            if ($sport != null && $level != null) {
+
+                try {
+                    
+                    $em->persist($favoriteSport);
+                    $em->flush();
+                    $this->addFlash('success', 'Le sport est rajouté dans vos favoris');
+        
+                }catch (\Exception $e) {
+                    
+                    $this->addFlash('warning','Ce sport fait déjà parti de votre liste de favori'); 
+                }
+            } else {
+                $this->addFlash('warning', 'renseigné tous les champs');
+            }
+
         }
 
         return $this->render('user_favorite_sport/index.html.twig', [
