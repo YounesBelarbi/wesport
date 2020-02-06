945f583ff37482666c14c265be09789f57df1bd6
younes <ybelarbi@outlook.com>
2020-02-06 13:50:16 +0100

    change name

---
+++ src/Form/EventOrganizationType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use App\Entity\Sport;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventOrganizationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('eventBody')
+            // ->add('author')
+            ->add('sportConcerned', EntityType::class, [
+                'class' => Sport::class,
+            ])
+            ->add('title')
+            ->add('location')
+            ->add('eventDate')
+            // ->add('createdAt')
+            // ->add('updatedAt')
+            // ->add('participatingUserList')
+            // ->add('eventOrganizer')
+            ->add('send', SubmitType::class, [
+                'label' => 'modifier'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
