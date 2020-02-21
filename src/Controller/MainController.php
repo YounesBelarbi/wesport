e1e4f97d2617a206183ad06320013bf604966f67
younes <ybelarbi@outlook.com>
2020-02-21 23:41:21 +0100

    get last events and classifiedAds from repository

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\ClassifiedAd;
+use App\Entity\Event;
+use App\Repository\EventRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +14,17 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="main")
      */
-    public function index()
+    public function index(EventRepository $eventRepository, EntityManagerInterface $em)
     {
+
+      
+
+      $lastEvent = $em->getRepository(Event::class)->lastEvents();
+      $lastClassifiedAds = $em->getRepository((ClassifiedAd::class))->lastClassifiedAds();
+
         return $this->render('main/index.html.twig', [
-          
+          'lastEvent' => $lastEvent,
+          'lastClassifiedAds' => $lastClassifiedAds,
         ]);
     }
 }
