3fc0cb1a7c6f53a8dc4e17edd2753bcb9a6b2cb4
younes <ybelarbi@outlook.com>
2020-02-21 23:43:21 +0100

    addition of repeatedType password and class for elmements

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,8 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,8 +19,13 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('username')
+            ->add('email',null, [
+                'row_attr' => ['class' => 'col-md-12'],
+            ])
+            ->add('username',null, [
+                'row_attr' => ['class' => 'col-md-12'],
+                'label' => 'pseudonyme'
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -27,22 +34,30 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
                 'mapped' => false,
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
+                        'minMessage' => 'Votre mots de passe doit contenir au minimum {{ limit }} caractères',
                         'max' => 4096,
-                    ]),
+                    ])
                 ],
+                'options' => ['row_attr' => ['class' => 'col-md-6']],
+                'first_options'  => [
+                    'label' => 'mot de passe',
+                    
+                ],
+                'second_options' => ['label' => 'Confirmer le mot de passe'],
+                'invalid_message' => 'Les mots de passe doivent être identiques',
             ])
+            ->add('send', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-lg btn-primary site-btn col-md-6'],
+                'label' => 'Je valide mon profile',
+                'row_attr' => ['class' => 'd-flex justify-content-center'],
+            ])
+            
         ;
     }
 
