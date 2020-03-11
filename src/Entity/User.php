bd182a5aff7fcbc59ef7fc3e479a5e8353852587
younes <ybelarbi@outlook.com>
2020-03-11 10:42:26 +0100

    groups addition

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
@@ -19,6 +20,7 @@ class User implements UserInterface
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"searchResult"})
      */
     private $id;
 
@@ -40,11 +42,13 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255, unique=true)
+     * @Groups({"searchResult"})
      */
     private $username;
 
     /**
      * @ORM\Column(type="integer", nullable=true)
+     * @Groups({"searchResult"})
      */
     private $age;
 
@@ -60,6 +64,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups({"searchResult"})
      */
     private $city;
 
