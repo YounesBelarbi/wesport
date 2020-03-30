b0d8c9436eacb781b5769b7be503e1f022c85602
younes <ybelarbi@outlook.com>
2020-03-30 20:08:57 +0200

    service's indentation

--- src/Service/SendMail.php
+++ src/Service/SendMail.php
@@ -2,31 +2,25 @@
 
 namespace App\Service;
 
-
 class SendMail
 {
-
     private $mailer;
 
-    public function __construct( \Swift_Mailer $mailer)
+    public function __construct(\Swift_Mailer $mailer)
     {
         $this->mailer = $mailer;
     }
 
 
-    public function sendAnEmail($subject, $mailFrom,$mailTo, $body)
+    public function sendAnEmail($subject, $mailFrom, $mailTo, $body)
     {
-
-
         $message = (new \Swift_Message($subject))
-        ->setFrom($mailFrom)
-        ->setTo($mailTo)
-        ->setBody(
-           $body
-        );
+            ->setFrom($mailFrom)
+            ->setTo($mailTo)
+            ->setBody(
+                $body
+            );
 
         $this->mailer->send($message);
-
     }
-
 }
