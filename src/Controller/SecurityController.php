6ae545dba88f8201612daa55c42d3b9f89380b22
younes <ybelarbi@outlook.com>
2020-03-30 19:36:39 +0200

    controllers's indentation

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -34,6 +34,7 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
+
     /**
      * @Route("/logout", name="app_logout")
      */
@@ -43,60 +44,58 @@ class SecurityController extends AbstractController
     }
 
 
-
-
     /**
      * @Route("/forgotten_password", name="app_forgotten_password")
      */
     public function forgottenPassword(Request $request, UserPasswordEncoderInterface $encoder, SendMail $sendMail, TokenGeneratorInterface $tokenGenerator): Response
     {
+        $form = $this->createForm(UserType::class);
+        $form->handleRequest($request);
 
-            $form = $this->createForm(UserType::class);
-            $form->handleRequest($request);
+        $email = $form->get('email')->getData();
 
-            $email = $form->get('email')->getData();
-        
-            
-            if ($form->isSubmitted() && $form->isValid()) {
-                
-                $entityManager = $this->getDoctrine()->getManager();
-                $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
-                
-                if ($user === null) {
-                    $this->addFlash('danger', 'Email Inconnu');
-                    return $this->redirectToRoute('app_forgotten_password');
-                }
-                $token = $tokenGenerator->generateToken();
+        if ($form->isSubmitted() && $form->isValid()) {
 
-                try{
-                    $user->setResetToken($token);
-                    $entityManager->flush();
-                } catch (\Exception $e) {
-                    $this->addFlash('warning', $e->getMessage());
-                    return $this->redirectToRoute('main');
-                }
+            $entityManager = $this->getDoctrine()->getManager();
+            $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
 
-                $url = $this->generateUrl('app_reset_password', array('token' => $token), UrlGeneratorInterface::ABSOLUTE_URL);
-                
-                //use service to send mail
-                $sendMail->sendAnEmail('Mot de passe oublié', $this->getParameter('app_email'), $user->getEmail(),  "Pour réinitialiser votre mot de passe cliquez sur le lien : " . $url,
-                'text/html');
-               
+            if ($user === null) {
+                $this->addFlash('danger', 'Email Inconnu');
+                return $this->redirectToRoute('app_forgotten_password');
+            }
+            $token = $tokenGenerator->generateToken();
 
-                $this->addFlash('success', 'Mail envoyé');
-
+            try {
+                $user->setResetToken($token);
+                $entityManager->flush();
+            } catch (\Exception $e) {
+                $this->addFlash('warning', $e->getMessage());
                 return $this->redirectToRoute('main');
             }
-        
+
+            $url = $this->generateUrl('app_reset_password', array('token' => $token), UrlGeneratorInterface::ABSOLUTE_URL);
+
+            //use service to send mail
+            $sendMail->sendAnEmail(
+                'Mot de passe oublié',
+                $this->getParameter('app_email'),
+                $user->getEmail(),
+                "Pour réinitialiser votre mot de passe cliquez sur le lien : " . $url,
+                'text/html'
+            );
+
+            $this->addFlash('success', 'Mail envoyé');
+            return $this->redirectToRoute('main');
+        }
+
 
         return $this->render('security/mail_user.html.twig', [
             'form' => $form->createView(),
-            'pageTitle' => 'Mot de passe oublié' ,
+            'pageTitle' => 'Mot de passe oublié',
             'title' => 'Mot de passe oubliée',
             'description' => 'saisissez votre email pour recevoir le lien vous permettant de changer votre mot de passe'
         ]);
     }
-
 
 
     /**
@@ -104,17 +103,13 @@ class SecurityController extends AbstractController
      */
     public function resetPassword(Request $request, string $token, UserPasswordEncoderInterface $passwordEncoder)
     {
-
-
         $form = $this->createForm(ResetPasswordType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-        
-            $entityManager = $this->getDoctrine()->getManager();
 
+            $entityManager = $this->getDoctrine()->getManager();
             $user = $entityManager->getRepository(User::class)->findOneBy(['resetToken' => $token]);
-            
 
             if ($user === null) {
                 $this->addFlash('danger', 'Un problème est survenu, votre mot de passe n\'a pas été modifié.');
@@ -128,14 +123,11 @@ class SecurityController extends AbstractController
             $this->addFlash('success', 'Votre mot de passe à bien été modifié');
 
             return $this->redirectToRoute('app_login');
-        }else {
+        } else {
 
             return $this->render('security/reset_password.html.twig',  [
                 'form' => $form->createView(),
             ]);
         }
-
     }
-
-
 }
--- src/Controller/UserFavoriteSportController.php
+++ src/Controller/UserFavoriteSportController.php
@@ -11,24 +11,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-    /**
-     * @Route("/user", name="user_favorite_")
-     */
+/**
+ * @Route("/user", name="user_favorite_")
+ */
 class UserFavoriteSportController extends AbstractController
 {
-
     /**
      * @Route("/listfavorite/sport", name="sport_list")
      */
     public function userFavoriteSportList(FavoriteSportRepository $favoriteSportRepository)
     {
         $userFavoriteSportList = $favoriteSportRepository->findBy(['user' => $this->getUser()]);
-        
+
         return $this->render('user_favorite_sport/user_favorite_sport_list.html.twig', [
             'userFavoriteSportList' => $userFavoriteSportList
         ]);
     }
-
 
 
     /**
@@ -36,16 +34,13 @@ class UserFavoriteSportController extends AbstractController
      */
     public function addFavoriteSport(Request $request, EntityManagerInterface $em)
     {
-        
         $user = $this->getUser();
-    
 
         $favoriteSportForm = $this->createForm(FavoriteSportType::class);
-
         $favoriteSportForm->handleRequest($request);
-       
+
         if ($favoriteSportForm->isSubmitted() && $favoriteSportForm->isValid()) {
-            
+
             $favoriteSport = new FavoriteSport;
             $level = $favoriteSportForm->get('level')->getData();
             $sport = $favoriteSportForm->get('sport')->getData();
@@ -57,20 +52,17 @@ class UserFavoriteSportController extends AbstractController
             if ($sport != null && $level != null) {
 
                 try {
-                    
                     $em->persist($favoriteSport);
                     $em->flush();
                     $this->addFlash('success', 'Le sport est rajouté dans vos favoris');
                     return $this->redirectToRoute('profile_edit');
-        
-                }catch (\Exception $e) {
-                    
-                    $this->addFlash('warning','Ce sport fait déjà parti de votre liste de favori'); 
+                } catch (\Exception $e) {
+
+                    $this->addFlash('warning', 'Ce sport fait déjà parti de votre liste de favori');
                 }
             } else {
                 $this->addFlash('warning', 'renseigné tous les champs');
             }
-
         }
 
         return $this->render('user_favorite_sport/favorite_sport.html.twig', [
@@ -84,37 +76,32 @@ class UserFavoriteSportController extends AbstractController
      */
     public function updateFavoriteSport(Request $request, $id, EntityManagerInterface $em)
     {
-        
-        $sport= $em->getRepository(Sport::class)->findOneBy(['id' => $id]);
+        $sport = $em->getRepository(Sport::class)->findOneBy(['id' => $id]);
         $user = $this->getUser();
         $myFavoriteSport = $em->getRepository(FavoriteSport::class)->find(['sport' => $sport, 'user' => $user]);
 
-
         $favoriteSportForm = $this->createForm(FavoriteSportType::class, $myFavoriteSport);
-
         $favoriteSportForm->handleRequest($request);
-       
-        if ($favoriteSportForm->isSubmitted() && $favoriteSportForm->isValid()){
+
+        if ($favoriteSportForm->isSubmitted() && $favoriteSportForm->isValid()) {
 
             $em->flush();
             $this->addFlash('success', 'Les modifications ont bien été faites');
             return $this->redirectToRoute('user_favorite_sport_list');
-
         }
 
         return $this->render('user_favorite_sport/favorite_sport.html.twig', [
             'favoriteSportForm' => $favoriteSportForm->createView(),
         ]);
-        
     }
 
 
     /**
      * @Route("/deletefavorite/sport/{id}", name="sport_delete")
      */
-    public function deleteFavoriteSport(Request $request,$id, EntityManagerInterface $em)
+    public function deleteFavoriteSport(Request $request, $id, EntityManagerInterface $em)
     {
-        $sport= $em->getRepository(Sport::class)->findOneBy(['id' => $id]);
+        $sport = $em->getRepository(Sport::class)->findOneBy(['id' => $id]);
         $user = $this->getUser();
         $myFavoriteSport = $em->getRepository(FavoriteSport::class)->find(['sport' => $sport, 'user' => $user]);
 
@@ -122,9 +109,5 @@ class UserFavoriteSportController extends AbstractController
         $em->flush();
         $this->addFlash('success', 'Les modifications ont bien été faites');
         return $this->redirectToRoute('user_favorite_sport_list');
-
-
     }
-
-
 }
