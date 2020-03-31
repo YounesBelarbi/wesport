82c1383a9498840c5185e4c840ae899046219b8f
younes <ybelarbi@outlook.com>
2020-03-31 19:17:47 +0200

    isActive field to true for role admin

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\ClassifiedAd;
 use App\Entity\Event;
 use App\Entity\FavoriteSport;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Nelmio\Alice\Loader\NativeLoader;
@@ -13,34 +14,34 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $em)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
         $loader = new Loader();
-        
+
         //importe le fichier de fixtures et récupère les entités générés
-        $entities = $loader->loadFile(__DIR__.'/fixtures.yaml')->getObjects();
-        
+        $entities = $loader->loadFile(__DIR__ . '/fixtures.yaml')->getObjects();
+
         //empile la liste d'objet à enregistrer en BDD
         foreach ($entities as $entity) {
 
-            if ($entity instanceof ClassifiedAd ) {
+            if ($entity instanceof ClassifiedAd) {
 
                 $entity->setAuthor($entity->getSeller()->getUsername());
-
             } elseif ($entity instanceof Event) {
+
                 $entity->setAuthor($entity->getEventOrganizer()->getUsername());
             }
 
-       
+            if ($entity instanceof User) {
 
+                if ($entity->getRoles() === ["ROLE_ADMIN", "ROLE_USER"]) {
+
+                    $entity->setIsActive(true);
+                }
+            }
 
             $em->persist($entity);
         };
-        
+
         //enregistre
         $em->flush();
-
-        
     }
 }
