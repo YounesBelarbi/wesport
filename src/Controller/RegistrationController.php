c8fe84372d8674d88b66a37b7cb3ab55046db0ec
younes <ybelarbi@outlook.com>
2020-04-01 15:46:46 +0200

    prevent access to the registration page for the logged-in user

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -21,6 +21,10 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, TokenGeneratorInterface $tokenGenerator, SendMail $sendMail): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('main');
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
