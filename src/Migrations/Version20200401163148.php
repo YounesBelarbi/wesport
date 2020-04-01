d57f43c8ed03d18bac699aa4b9f3013e975a9bcc
younes <ybelarbi@outlook.com>
2020-04-01 19:20:44 +0200

    new migration

--- src/Migrations/Version20200401163148.php
+++ src/Migrations/Version20200401163148.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200225140205 extends AbstractMigration
+final class Version20200401163148 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -24,6 +24,7 @@ final class Version20200225140205 extends AbstractMigration
 
         $this->addSql('CREATE TABLE favorite_sport (user_id INT NOT NULL, sport_id INT NOT NULL, level_id INT DEFAULT NULL, INDEX IDX_AC0DC4F6A76ED395 (user_id), INDEX IDX_AC0DC4F6AC78BCF8 (sport_id), INDEX IDX_AC0DC4F65FB14BA7 (level_id), PRIMARY KEY(user_id, sport_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE classified_ad (id INT AUTO_INCREMENT NOT NULL, seller_id INT NOT NULL, classified_ad_body LONGTEXT NOT NULL, title VARCHAR(255) NOT NULL, author VARCHAR(255) NOT NULL, sport_concerned VARCHAR(255) NOT NULL, object_for_sale VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, price DOUBLE PRECISION NOT NULL, INDEX IDX_6869CB768DE820D9 (seller_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_token (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, type VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, token VARCHAR(255) NOT NULL, INDEX IDX_BDF55A63A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, age INT DEFAULT NULL, last_name VARCHAR(255) DEFAULT NULL, first_name VARCHAR(255) DEFAULT NULL, city VARCHAR(255) DEFAULT NULL, phone_number VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL, reset_token VARCHAR(255) DEFAULT NULL, is_active TINYINT(1) NOT NULL, confirmation_token VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user_event (user_id INT NOT NULL, event_id INT NOT NULL, INDEX IDX_D96CF1FFA76ED395 (user_id), INDEX IDX_D96CF1FF71F7E88B (event_id), PRIMARY KEY(user_id, event_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user_contact_list (user_id INT NOT NULL, contact_list_id INT NOT NULL, INDEX IDX_ECFB06E5A76ED395 (user_id), INDEX IDX_ECFB06E5A781370A (contact_list_id), PRIMARY KEY(user_id, contact_list_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -36,6 +37,7 @@ final class Version20200225140205 extends AbstractMigration
         $this->addSql('ALTER TABLE favorite_sport ADD CONSTRAINT FK_AC0DC4F6AC78BCF8 FOREIGN KEY (sport_id) REFERENCES sport (id)');
         $this->addSql('ALTER TABLE favorite_sport ADD CONSTRAINT FK_AC0DC4F65FB14BA7 FOREIGN KEY (level_id) REFERENCES level (id)');
         $this->addSql('ALTER TABLE classified_ad ADD CONSTRAINT FK_6869CB768DE820D9 FOREIGN KEY (seller_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE user_token ADD CONSTRAINT FK_BDF55A63A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE user_event ADD CONSTRAINT FK_D96CF1FFA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_event ADD CONSTRAINT FK_D96CF1FF71F7E88B FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_contact_list ADD CONSTRAINT FK_ECFB06E5A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
@@ -53,6 +55,7 @@ final class Version20200225140205 extends AbstractMigration
 
         $this->addSql('ALTER TABLE favorite_sport DROP FOREIGN KEY FK_AC0DC4F6A76ED395');
         $this->addSql('ALTER TABLE classified_ad DROP FOREIGN KEY FK_6869CB768DE820D9');
+        $this->addSql('ALTER TABLE user_token DROP FOREIGN KEY FK_BDF55A63A76ED395');
         $this->addSql('ALTER TABLE user_event DROP FOREIGN KEY FK_D96CF1FFA76ED395');
         $this->addSql('ALTER TABLE user_contact_list DROP FOREIGN KEY FK_ECFB06E5A76ED395');
         $this->addSql('ALTER TABLE event DROP FOREIGN KEY FK_3BAE0AA76A7F4729');
@@ -65,6 +68,7 @@ final class Version20200225140205 extends AbstractMigration
         $this->addSql('ALTER TABLE level_sport DROP FOREIGN KEY FK_705CEADBAC78BCF8');
         $this->addSql('DROP TABLE favorite_sport');
         $this->addSql('DROP TABLE classified_ad');
+        $this->addSql('DROP TABLE user_token');
         $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE user_event');
         $this->addSql('DROP TABLE user_contact_list');
