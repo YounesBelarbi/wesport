db3b91106d5a3057105ec783799ba5b7a3e7aa66
younes <ybelarbi@outlook.com>
2020-04-03 15:51:17 +0200

    factorization of token service

--- src/Service/TokenManager.php
+++ src/Service/TokenManager.php
@@ -23,37 +23,31 @@ class TokenManager
 
     public function generateAndSaveToken(string $type, User $user)
     {  
-        //check if user already has token for this type.  
+        //check if user already has token for this type.
         $userToken = $this->entityManager->getRepository(UserToken::class)->findOneBy(['user' => $user, 'type' => $type]);
 
         if ($userToken) {
-            // if yes, generate new token save and send it.
+            
+            // if yes, generate new token to replace and send it.
             $token = $this->tokenGenerator->generateToken();
             $userToken->setToken($token);
             $this->entityManager->flush(); 
         } else {
-            //if no, generate and save new token for this user
-            $token = $this->newUserToken($type,$user);
+
+            //if no, generate and save new token for this user            
+            $token = $this->tokenGenerator->generateToken();
+            $userToken = new UserToken;
+            $currentDate = new \DateTime();
+
+            $userToken->setToken($token);
+            $userToken->setType($type);            
+            $userToken->setUser($user);
+            $userToken->setCreatedAt(new \DateTime());
+            $userToken->setExpirationDate($currentDate->modify( '+1 month'));
+            
+            $this->entityManager->persist($userToken);
+            $this->entityManager->flush(); 
         }
         return $token;
     }
-    
-
-    public function newUserToken(string $type, User $user)
-    {
-        $token = $this->tokenGenerator->generateToken();
-        $userToken = new UserToken;
-        $currentDate = new \DateTime();
-
-        $userToken->setToken($token);
-        $userToken->setType($type);            
-        $userToken->setUser($user);
-        $userToken->setCreatedAt(new \DateTime());
-        $userToken->setExpirationDate($currentDate->modify( '+1 month'));
-        
-        $this->entityManager->persist($userToken);
-        $this->entityManager->flush(); 
-
-        return $token;
-    }    
 }
