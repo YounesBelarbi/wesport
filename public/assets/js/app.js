27e7dd0955ad6ff1dcbef093e3e38a3ea7ca8b69
younesbelarbi <ybelarbi@outlook.com>
2020-07-08 23:39:42 +0200

    process to get select field value

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,9 +1,19 @@
 let app = {
   init: function () {
     console.log("init");
+    $("select").change(app.handleSelect);
     setTimeout(function () {
       $(".alert").remove();
     }, 5000);
+  },
+  handleSelect: function (e) {
+    // e.preventDefault();
+    var infoObject = {};
+    $("select option:selected").each(function () {
+      let fieldName = $(this).parent().attr("id");
+      infoObject[fieldName] = $(this).text();
+    });
+    console.log(infoObject);
   },
 };
 $(app.init);
