3b5efde57d298393078cabd9cb040dd7ec037e7d
younesbelarbi <ybelarbi@outlook.com>
2020-07-09 16:22:09 +0200

    js and css move to the assets webpack folder

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,4 +11,24 @@ import '../css/app.css';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+import wesport from "./wesport.js";
+
+let app = {
+    init: function () {
+        console.log("init");
+        $("select").change(app.handleSelect);
+        setTimeout(function () {
+            $(".alert").remove();
+        }, 5000);
+    },
+    handleSelect: function (e) {
+        // e.preventDefault();
+        var infoObject = {};
+        $("select option:selected").each(function () {
+            let fieldName = $(this).parent().attr("id");
+            infoObject[fieldName] = $(this).text();
+        });
+        console.log(infoObject);
+    },
+};
+$(app.init);
