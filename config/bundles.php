20a5edd3f89fe157b7e2ffa20d19bbf9f3e5b223
younesbelarbi <ybelarbi@outlook.com>
2020-07-25 17:50:16 +0200

    vichUploaderBundle installation

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
