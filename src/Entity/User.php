762aeae52d27e000fd7bcde9371c177a6aa00f23
younesbelarbi <ybelarbi@outlook.com>
2020-07-27 13:03:52 +0200

    add image name to groups

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,8 +6,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -120,6 +120,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups({"searchResult"})
      */
     private $profileImage;
 
