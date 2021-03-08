80692a645c9c4301a87f7fe67efec9a6a496c883
younes <ybelarbi.dev@gmail.com>
2021-03-08 11:18:52 +0100

    unnecessary code

--- src/Service/RequestCity.php
+++ src/Service/RequestCity.php
@@ -9,43 +9,23 @@ class RequestCity
     public function getDepartement()
     {
         $httpClient = HttpClient::create();
-        $response = $httpClient->request('GET', 'https://geo.api.gouv.fr/departements');    
+        $response = $httpClient->request('GET', 'https://geo.api.gouv.fr/departements');
         $choices = [];
 
         foreach ($response->toArray() as $key => $choice) {
-        $choices[$choice['nom']] = $choice['code'];
-        }     
-        
-        ksort($choices);        
+            $choices[$choice['nom']] = $choice['code'];
+        }
+
+        ksort($choices);
         return $choices;
     }
 
-    
+
     public function getCity($id)
     {
         $httpClient = HttpClient::create();
-        $response = $httpClient->request('GET', "https://geo.api.gouv.fr/departements/".$id."/communes");
-        $departementCytiesName = [];
- 
+        $response = $httpClient->request('GET', "https://geo.api.gouv.fr/departements/" . $id . "/communes");
+
         return $response->toArray();
-    } 
-
-
-    public function cityPerDepartement() {    
-    
-        // $httpClient = HttpClient::create();
-        // $departement = $httpClient->request('GET', 'https://geo.api.gouv.fr/departements');
-
-        // $arrayDepartement =  $departement->toArray();
-
-        // foreach ($arrayDepartement as $key => $value) {
-
-        //     $citiesDepartement[$value['nom']]= $this->getCity($value['code']);
-        // }
-        // return $citiesDepartement;
     }
-
 }
-
-
-
