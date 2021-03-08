2ff99cb3662ff82e9a4f782fd0a95d4d47513cc4
younes <ybelarbi.dev@gmail.com>
2021-03-08 11:15:41 +0100

    console.log deletion

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,52 +3,48 @@ const axios = require("axios").default;
 let app = {
     init: function () {
         $(".select").change(app.handleSelect);
-        
+
         setTimeout(function () {
             $(".alert").remove();
         }, 5000);
-        
-        $( ".city" ).focus(function() {
+
+        $(".city").focus(function () {
             app.handleCityList();
         });
 
-        $( ".formSearch" ).submit(function(e) {
+        $(".formSearch").submit(function (e) {
             e.preventDefault();
-        }); 
+        });
     },
-    data:{city:null, sport:null},
+    searchFormData: { city: null, sport: null },
     handleSelect: function (e) {
         //get select fields data and use it in axios request 
         $(".select option:selected").each(function () {
             let fieldName = $(this).parent().attr("data-name");
 
-                app.data[fieldName] = $(this).text();
-                });
-                console.log('champ sport:', app.data)
-        app.searchRequestUsers(app.data);
-        console.log(app.data)
+            app.searchFormData[fieldName] = $(this).text();
+        });
+        app.searchRequestUsers(app.searchFormData);
     },
     handleCitySelect: function (value) {
 
-        if (value) {            
-            app.data['city'] = value;
-            app.searchRequestUsers(app.data);          
+        if (value) {
+            app.searchFormData['city'] = value;
+            app.searchRequestUsers(app.searchFormData);
         }
-        console.log('champ ville:', app.data)
-       
     },
     searchRequestUsers: function (data) {
         axios
-        .post("/sportresearch", data)
-        .then(function (response) {
-            app.generateListElement(response);
+            .post("/sportresearch", data)
+            .then(function (response) {
+                app.generateListElement(response);
             })
-        .catch(function (error) {
-            //if the user is not authenticated 
-            if (error.response.status == 403) {
-                location.href = '/login';
-            }
-        });
+            .catch(function (error) {
+                //if the user is not authenticated 
+                if (error.response.status == 403) {
+                    location.href = '/login';
+                }
+            });
     },
     generateListElement: function (response) {
         let $section = $("#result_section");
@@ -58,20 +54,20 @@ let app = {
 
         let $templateElement = $("#templateResult");
         let $contentElements = $templateElement.contents();
-        
+
         let $result = response.data;
         let resultNumber = $result.length;
         $result_container.append("<div class='d-flex justify-content-between m-4 row'><h2  class='search-title'> il y a " + resultNumber + " résultat pour votre recherche</h2></div>");
-        
+
         $.each(response.data, function (key, value) {
-            
+
             //clone template
             let $cloneContentElements = $contentElements.clone();
             // filling the template with the information
             $cloneContentElements.find('.username').text(value.username)
             $cloneContentElements.find('.age').text(value.age + ' ans')
             $cloneContentElements.find('.user-city').text(value.city)
-            
+
 
             //insert template in dom
             $result_container.append($cloneContentElements)
@@ -83,57 +79,32 @@ let app = {
             source: function (request, response) {
                 axios({
                     method: 'get',
-                    url: "https://api-adresse.data.gouv.fr/search/?city="+$(".city").val(),
+                    url: "https://api-adresse.data.gouv.fr/search/?city=" + $(".city").val(),
                     params: {
-                        q: request.term, limit: 4,importance:1
-                      },
+                        q: request.term, limit: 4, importance: 1
+                    },
                     responseType: "json"
-                  })
+                })
                     .then(function (data) {
                         var cities = [];
                         response($.map(data.data.features, function (item) {
                             // Ici on est obligé d'ajouter les villes dans un array pour ne pas avoir plusieurs fois la même
                             if ($.inArray(item.properties.postcode, cities) == -1) {
-                                cities.push(item.properties.postcode);                              
-                                 
-                                return { label:item.properties.city, 
+                                cities.push(item.properties.postcode);
+
+                                return {
+                                    label: item.properties.city,
                                     city: item.properties.city,
                                     value: item.properties.city
-                                    };                          
+                                };
                             }
                         }));
                     });
-            } ,minLength: 3,
-            select: function(event, ui) {
-                app.handleCitySelect( ui.item.value);
+            }, minLength: 3,
+            select: function (event, ui) {
+                app.handleCitySelect(ui.item.value);
             }
         });
-    },
-    // handleCityList: function () {
-    //     axios
-    //         .post("/sportresearch/get-city-list", { 'departmentCode': $(".departement").val() })
-    //         .then(function (response) {
-    //             app.fillFormField(response);
-    //         })
-    //         .catch(function (error) {
-    //             //if  API server returned error
-    //             if (error.response.data.status == 500) {
-    //                 let $section = $("#search_section");
-    //                 $section.append('<p class= "alert alert-warning">un problème est survenue relancer la recherche</p>');
-
-    //             }
-
-    //         });
-    // },
-    // fillFormField: function (response) {
-    //     $(".city option").remove(); //reset previous result of select field
-    //     $('.city').prop("disabled", false); //field city select activation
-
-    //     $('.city').append('<option>Ville</option>');
-    //     $.each(response.data.cityList, function (key, value) {
-
-    //         $('.city').append('<option value="' + value.nom + '">' + value.nom + '</option>');
-    //     });
-    // }
+    }
 };
 $(app.init);
