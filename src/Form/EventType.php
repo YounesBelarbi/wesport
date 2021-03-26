e613b69bded8ab91444389f907b2efda524c3101
younes <ybelarbi.dev@gmail.com>
2021-03-26 19:00:51 +0100

    fix: the form to modify an event did not load the event
    modify the form by loading the sport from the form builder and adding a form event (form used for creating and modifying events)

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -4,11 +4,14 @@ namespace App\Form;
 
 use App\Entity\Event;
 use App\Entity\Sport;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class EventType extends AbstractType
@@ -19,11 +22,6 @@ class EventType extends AbstractType
             ->add('eventBody', null, [
                 'row_attr' => ['class' => 'col-md-12'],
                 'label' => 'Texte déscriptif de l\'événement',
-            ])
-            ->add('sportConcerned', EntityType::class, [
-                'class' => Sport::class,
-                'row_attr' => ['class' => 'col-md-4'],
-                'label' => 'Sport',
             ])
             ->add('title', null, [
                 'row_attr' => ['class' => 'col-md-12'],
@@ -43,7 +41,37 @@ class EventType extends AbstractType
                 'attr' => ['class' => 'btn btn-lg btn-primary site-btn col-md-6'],
                 'label' => 'Enregistrer l\'événement',
                 'row_attr' => ['class' => 'd-flex justify-content-center'],
-            ]);
+            ])
+            ->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event) {
+                $form = $event->getForm();
+
+                if ($event->getData()) {
+                    $form
+                    ->add('sportConcerned', EntityType::class, [
+                        'class' => Sport::class,
+                        'row_attr' => ['class' => 'col-md-4'],
+                        'mapped' => false,
+                        'label' => 'Sport',
+                        'placeholder' => $event->getData()->getSportConcerned(),
+                        'query_builder' => function (EntityRepository $er) {
+                            return $er->createQueryBuilder('s')
+                            ->orderBy('s.name', 'ASC');
+                        },
+                    ]);                    
+                } else {
+                    $form
+                    ->add('sportConcerned', EntityType::class, [
+                        'class' => Sport::class,
+                        'row_attr' => ['class' => 'col-md-4'],
+                        'mapped' => false,
+                        'label' => 'Sport',
+                        'query_builder' => function (EntityRepository $er) {
+                            return $er->createQueryBuilder('s')
+                            ->orderBy('s.name', 'ASC');
+                        },
+                    ]);
+                }                 
+            });
     }
 
     public function configureOptions(OptionsResolver $resolver)
